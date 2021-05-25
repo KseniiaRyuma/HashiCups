9ef0663fc17c4e23167d4cf5a3ec6e3821e5f12a
Kseniia Ryuma <kseniia@hashicorp.com>
2021-05-25 17:48:01 -0400

    Update variables.tf

--- setup/terraform/tfc-workspaces/variables.tf
+++ setup/terraform/tfc-workspaces/variables.tf
@@ -2,4 +2,4 @@ variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 variable "TFC_ORGANIZATION" {}
 variable "GITHUB_TOKEN" {}
-variable "GITHUB_IDENTIFIER"
+variable "GITHUB_IDENTIFIER" {}
